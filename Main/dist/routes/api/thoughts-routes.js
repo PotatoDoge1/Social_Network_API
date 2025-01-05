9def808cc87e3d4ff8af574ddfcf963b6d00b7e6
PotatoDoge1 <ryan.a.schwark@gmail.com>
2025-01-05 13:14:32 -0500

    initial commit

---
+++ Main/dist/routes/api/thoughts-routes.js
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+const router = Router();
+// import {
+//     getThoughts
+// } from 
+export { router as thoughtsRoutes };
---
+++ Main/src/routes/api/thoughts-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// import {
+//     getThoughts
+// } from 
+
+export { router as thoughtsRoutes };
