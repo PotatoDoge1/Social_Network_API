42afa872c615abd6c6389b6ba487020dfc635d69
PotatoDoge1 <81260899+PotatoDoge1@users.noreply.github.com>
2025-01-05 17:50:12 -0500

    Delete Main/dist directory

--- Main/dist/api/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-//import { userRoutes } from './user-routes.ts';
-//import { userThoughts } from './thoughts-routes.ts';
-const router = Router();
-//router.use('/users', userRoutes);
-//router.use('/thoughts', thoughtsRoutes);
-export default router;
--- Main/dist/api/thoughts-routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-// import {
-//     getThoughts
-// } from 
-export { router as thoughtsRoutes };
