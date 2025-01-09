247a8066a8edeed8427b89616480d8944beb8ce8
PotatoDoge1 <ryan.a.schwark@gmail.com>
2025-01-09 18:59:57 -0500

    added controllers and routes to add and delete friends and update user info

--- Main/src/controllers/userController.ts
+++ Main/src/controllers/userController.ts
@@ -57,3 +57,67 @@ export const deleteUserById = async(req: Request, res: Response) => {
         return res.status(500).json(error);
     }
 }
+
+// update a user
+export const updateUserById = async(req: Request, res: Response) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            {
+                runValidators: true,
+                new: true
+            }
+        )
+
+        if (!user) {
+            return res.status(404).json({ message: 'No user with this ID.'});
+        }
+
+        return res.json(user);
+
+    } catch (error) {
+        console.log("Error updating user by ID.");
+        return res.status(500).json(error);
+    }
+}
+
+// add friend
+export const addFriend = async(req: Request, res: Response) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true }
+        );
+
+        if (!userData) {
+            return res.status(404).json({ message: "Error adding friend." });
+        }
+
+        return res.json(userData);
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+}
+
+// delete friend
+export const deleteFriend = async(req: Request, res: Response) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        );
+
+        if (!userData) {
+            return res.status(404).json({ message: "Error deleting friend." });
+        }
+
+        return res.json(userData);
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+}
